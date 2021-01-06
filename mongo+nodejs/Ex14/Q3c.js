c810bd5eafd811f69705f38836b095367d430958
ManishS6 <63586933+ManishS6@users.noreply.github.com>
2021-01-06 15:57:12 +0530

    MongoDB updateOne

---
+++ mongo+nodejs/Ex14/Q3c.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://manishSwami:manish123@cluster0.ldvmn.mongodb.net/first?retryWrites=true&w=majority";
+MongoClient.connect(url,{useUnifiedTopology:true}, function(err, db) {
+  if (err) throw err;
+  var dbo=db.db("first");
+  var myquery= {address: "Highway 37"};
+  var newvalues = {$set: {name:"Google Inc", address:"Somewhere in america"}};
+  dbo.collection("a").updateOne(myquery,newvalues, function(err, res)
+  {
+    if (err) throw err;
+    console.log("1 document has been updated");
+    db.close();
+  });
+});
