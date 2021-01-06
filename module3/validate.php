c9b556bb02695f590bb281e151c31f64b965d451
ManishS6 <63586933+ManishS6@users.noreply.github.com>
2021-01-06 18:54:05 +0530

    Simple Form Validation Completed

---
+++ module3/validate.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        function formValidate(){
+            var a = document.forms["Form"]["firstname"].value;
+            if(a==""){
+            alert("Name field cannot be empty");
+            return false;
+            }
+        }
+    </script>
+</head>
+<body>
+    <form name="Form" action="/module4/validate.php" onsubmit="return formValidate()" method="POST">
+        Name: <input type="text" name="firstname">
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
