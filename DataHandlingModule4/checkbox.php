91fc001e913dcc6c11170a46b6f1002d2a317c85
ManishS6 <63586933+ManishS6@users.noreply.github.com>
2021-01-08 15:55:54 +0530

    Hack to php

--- DataHandlingModule4/checkbox.php
+++ DataHandlingModule4/checkbox.php
@@ -1,16 +1,19 @@
-<html>
-<head>
-<title>Manish Swami 19bce1382</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Manish Swami 19bce1382</title>
+    </head>
 <body>
-<form action="RESULTcheckbox.php" method="post">
-<h1>Select the name of the fruit you want to buy</h1>
-<input type="checkbox" name="chk1[]" value="Banana"><label>Banana</label><br>
-<input type="checkbox" name="chk1[]" value="Apple"><label>Apple</label><br>
-<input type="checkbox" name="chk1[]" value="Orange"><label>Orange</label><br>
-<input type="checkbox" name="chk1[]" value="Pomo"><label>Pomo</label><br>
-<input type="checkbox" name="chk1[]" value="Grapes"><label>Grapes</label><br>
-<input type="submit" name="Submit" value="Submit"/>
-</form>
+    <form action="RESULTcheckbox.php" method="post">
+    <h1>Select the name of the fruit you want to buy</h1>
+    <input type="checkbox" name="chk1[]" value="Banana"><label>Banana</label><br>
+    <input type="checkbox" name="chk1[]" value="Apple"><label>Apple</label><br>
+    <input type="checkbox" name="chk1[]" value="Orange"><label>Orange</label><br>
+    <input type="checkbox" name="chk1[]" value="Pomo"><label>Pomo</label><br>
+    <input type="checkbox" name="chk1[]" value="Grapes"><label>Grapes</label><br>
+    <input type="submit" name="Submit" value="Submit"/>
+    </form>
 </body>
 </html>
